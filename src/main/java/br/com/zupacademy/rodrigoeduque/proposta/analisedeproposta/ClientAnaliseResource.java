c9cc0ab1a6da6d58ddfa36b0d8e5e041b5a65cf5
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-07-23 17:04:57 -0300

    Feature - 040 - Vamos Rodar Nossa Aplicação

--- src/main/java/br/com/zupacademy/rodrigoeduque/proposta/analisedeproposta/ClientAnaliseResource.java
+++ src/main/java/br/com/zupacademy/rodrigoeduque/proposta/analisedeproposta/ClientAnaliseResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "http://localhost:9999/api", name = "analiseDeCreditoCliente")
+@FeignClient(url = "${analise.financeira.url}", name = "analiseDeCreditoCliente")
 public interface ClientAnaliseResource {
 
     @RequestMapping(method = RequestMethod.POST, value = "/solicitacao", consumes = "application/json")
--- src/main/java/br/com/zupacademy/rodrigoeduque/proposta/associarcartao/ClientCartoes.java
+++ src/main/java/br/com/zupacademy/rodrigoeduque/proposta/associarcartao/ClientCartoes.java
@@ -3,7 +3,7 @@ package br.com.zupacademy.rodrigoeduque.proposta.associarcartao;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "solicitaCartao", url = "http://localhost:8888/api/")
+@FeignClient(name = "solicitaCartao", url = "${cartao.url}")
 public interface ClientCartoes {
 
     @RequestMapping(method = RequestMethod.GET, value = "cartoes" )
