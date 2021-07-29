a44788f1f622ed967a9209571032e8bfbf721509
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-07-29 11:04:09 -0300

    Feature 095 - As coisas podem falhar durante as chamadas. Como posso identificar isso?

---
+++ src/main/java/br/com/zupacademy/rodrigoeduque/proposta/tracing/FeignTracingAutoConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.rodrigoeduque.proposta.tracing;
+
+import feign.Client;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(Client.class)
+@ConditionalOnBean(Tracer.class)
+@AutoConfigureAfter(TracerAutoConfiguration.class)
+@AutoConfigureBefore(name = "org.springframework.cloud.openfeign.FeignAutoConfiguration")
+@ConditionalOnProperty(name = "opentracing.spring.cloud.feign.enabled", havingValue = "true", matchIfMissing = true)
+public class FeignTracingAutoConfiguration {
+
+}
