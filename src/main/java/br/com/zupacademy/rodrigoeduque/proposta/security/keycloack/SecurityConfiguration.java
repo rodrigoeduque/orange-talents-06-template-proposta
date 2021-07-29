09483ce5fe4f0dacd6f7c48889e63f6b87991957
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-07-29 09:49:02 -0300

    correções e implementação correta da feature de Login com senha, onde não havia usado o oAuth2, só fui perceber o vacilo após tentar implementar o jeager

---
+++ src/main/java/br/com/zupacademy/rodrigoeduque/proposta/security/keycloack/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.rodrigoeduque.proposta.security.keycloack;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+        .antMatchers(HttpMethod.GET,"/propostas/**").hasAuthority("SCOPE_escopo-app-proposta")
+        .antMatchers(HttpMethod.POST,"/propostas/**").hasAuthority("SCOPE_escopo-app-proposta")
+        .antMatchers(HttpMethod.GET,"/cartoes/**").hasAuthority("SCOPE_escopo-app-proposta")
+        .antMatchers(HttpMethod.POST,"/cartoes/**").hasAuthority("SCOPE_escopo-app-proposta")
+        .anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
